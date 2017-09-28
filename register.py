f5c8e839c9c0a58c2edb6073b989e7f08379dd10
schultzm <dr.mark.schultz@gmail.com>
2017-09-28 11:15:01 +1000

    Updating readme

--- register.py
+++ register.py
@@ -1,24 +1,11 @@
 import pandoc
 import os
 
-pandoc.core.PANDOC_PATH = '/usr/bin/pandoc'
+# pandoc.core.PANDOC_PATH = '/usr/bin/pandoc'
 
 doc = pandoc.Document()
 doc.markdown = open('README.md').read()
-f = open('README.txt','w+')
-f.write(doc.rst)
-f.close()
+with open('README.txt','w+') as f:
+    f.write(doc.rst)
 os.system("setup.py register")
 os.remove('README.txt')
-
-
-
-try:
-    long_description = pypandoc.convert('README.md', 'rst')
-    long_description = long_description.replace("\r","") # YOU  NEED THIS LINE
-except OSError:
-    print("Pandoc not found. Long_description conversion failure.")
-    import io
-    # pandoc is not installed, fallback to using raw contents
-    with io.open('README.md', encoding="utf-8") as f:
-        long_description = f.read()
