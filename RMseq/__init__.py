fc1fa81e206a5f080d1b1e16c060260c80400776
Romain <romain.guerillot@hotmail.fr>
2017-10-03 13:42:59 +1100

    update readme, update check

--- RMseq/__init__.py
+++ RMseq/__init__.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __name__            = 'rmseq'
 __test_data__    = 'test_data'
 __parent_dir__   = 'RMseq'
-__version__         = '0.0.39b'
+__version__         = '0.0.40b'
 __version_date__    = 'Tue 3 October, 2017'
 __description__     = 'RM-seq is a bioinformatics tool for for ' +\
                        'assessing resistance mutations from PE short-reads.'
